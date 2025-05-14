1b086a9d632372c29ae2376adfd6669d8b149fa3
LuisFernando93 <39248747+LuisFernando93@users.noreply.github.com>
2025-05-14 16:42:49 -0300

    Player added. PlayerShift script added

---
+++ Assets/Scripts/PlayerShift.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlayerShift : MonoBehaviour
+{
+    private bool isCar = true;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isCar)
+            {
+                ShiftToPlane();
+            }
+            else if (!isCar)
+            {
+                ShiftToCar();
+            }
+        }
+    }
+
+    private void ShiftToCar()
+    {
+        isCar = true;
+        Debug.Log("Agora sou um carro");
+    }
+
+    private void ShiftToPlane()
+    {
+        isCar = false;
+        Debug.Log("Agora sou um aviao");
+    }
+}
